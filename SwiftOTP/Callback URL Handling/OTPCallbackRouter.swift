a07723dd7c76ed231f0ab11b5ebc3052fd30b98e
Bruno Philipe <git@bruno.ph>
2019-12-22 15:11:29 +0100

    Remove unused code

--- SwiftOTP/Callback URL Handling/OTPCallbackRouter.swift
+++ SwiftOTP/Callback URL Handling/OTPCallbackRouter.swift
@@ -208,34 +208,8 @@ enum CallbackError: Int, FailureCallbackError
 	}
 }
 
-private extension TokenStore
-{
-	func loadToken(with accountHash: String) -> Token?
-	{
-		for tokenIndex in 0..<count
-		{
-			if let tokenHash = loadAccount(at: tokenIndex),
-				tokenHash.data(using: .ascii)?.sha256Hash == accountHash.data(using: .ascii)
-			{
-				return load(tokenIndex)
-			}
-		}
-
-		return nil
-	}
-}
-
 private extension Data
 {
-	var sha256Hash: Data
-	{
-		var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
-		withUnsafeBytes {
-			_ = CC_SHA256($0, CC_LONG(count), &hash)
-		}
-		return Data(bytes: hash)
-	}
-
 	static func random(length: Int) -> Data?
 	{
 		var keyData = Data(count: length)
